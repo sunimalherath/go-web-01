261319b2edfb3be3418e0f9694c43162f278ffc8
sunimalherath <sunimaldv@gmail.com>
2021-12-08 23:31:05 +1100

    refactoring code

--- main.go
+++ main.go
@@ -1,29 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":8080"
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template: ", err)
-		return
-	}
-}
 
 func main() {
 	http.HandleFunc("/", Home)
